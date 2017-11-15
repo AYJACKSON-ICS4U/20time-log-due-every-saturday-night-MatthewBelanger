902670b50a38d63b6f003b10882c8f7625301377
MatthewBelanger <31661435+MatthewBelanger@users.noreply.github.com>
2017-11-14 19:47:22 -0500

    moved Tetris files

---
+++ Tetris/Tetris GameScene file week 7.swift
@@ -0,0 +1,55 @@
+//
+//  GameScene.swift
+//  Matt's Tetris
+//
+//  Created by Matthew Belanger on 2017-11-02.
+//  Copyright © 2017 Matthew Belanger. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+let TickLengthLevelOne = TimeInterval(600)
+
+class GameScene: SKScene {
+    
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        addChild(background)
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        /* Called before rendering each frame */
+        guard let lastTick = lastTick else {
+            return
+        }
+        let timePassed = lastTick.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis {
+            self.lastTick = NSDate()
+            tick?()
+        }
+    }
+    
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
+    }
+}
