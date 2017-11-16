3c58787459d2be58e22331dce5f56d25394b1efc
MatthewBelanger <31661435+MatthewBelanger@users.noreply.github.com>
2017-11-15 21:03:17 -0500

    add game directory and files

---
+++ Tetris/Game/Array2D.swift
@@ -0,0 +1,29 @@
+//
+//  Array2D.swift
+//  Matt's Tetris
+//
+//  Created by Matthew Belanger on 2017-11-02.
+//  Copyright © 2017 Matthew Belanger. All rights reserved.
+//
+
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
