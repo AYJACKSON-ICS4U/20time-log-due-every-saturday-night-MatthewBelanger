46b14ec555a9a1fe6b8f8506347fdd32164da2d6
MatthewBelanger <31661435+MatthewBelanger@users.noreply.github.com>
2017-11-26 10:41:51 -0500

    Added rules for falling blocs

--- Tetris/Game/Matt's Tetris/Shape.swift
+++ Tetris/Game/Matt's Tetris/Shape.swift
@@ -124,11 +124,34 @@ class Shape: Hashable, CustomStringConvertible {
             blocks[idx].row = row + diff.rowDiff
         }
     }
+    final func rotateClockwise() {
+        let newOrientation = Orientation.rotate(orientation: orientation, clockwise: true)
+        rotateBlocks(orientation: newOrientation)
+        orientation = newOrientation
+    }
+    
+    final func rotateCounterClockwise() {
+        let newOrientation = Orientation.rotate(orientation: orientation, clockwise: false)
+        rotateBlocks(orientation: newOrientation)
+        orientation = newOrientation
+    }
     
     final func lowerShapeByOneRow() {
         shiftBy(columns: 0, rows:1)
     }
     
+    final func raiseShapeByOneRow() {
+        shiftBy(columns: 0, rows:-1)
+    }
+    
+    final func shiftRightByOneColumn() {
+        shiftBy(columns: 1, rows:0)
+    }
+    
+    final func shiftLeftByOneColumn() {
+        shiftBy(columns: -1, rows:0)
+    }
+
     
     final func shiftBy(columns: Int, rows: Int) {
         self.column += columns
